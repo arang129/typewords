7b6e921a4c099309d515ee890170928f44d16a8f
arang129 <48246455+arang129@users.noreply.github.com>
2024-02-29 10:28:27 +0800

    Update hello_jupyter_proxy.py

--- hello_jupyter_proxy.py
+++ hello_jupyter_proxy.py
@@ -1,9 +1,5 @@
 """A minimal example server to run with jupyter-server-proxy
 """
-import os
-import sqlite3
-import urllib.parse
-
 import argparse
 import socket
 import sys
@@ -12,21 +8,6 @@ from pathlib import Path
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 __version__ = '0.2'
-
-# 在類定義中新增
-DATABASE_PATH = '/home/jupyter-劉文讓/data.db'
-
-def get_username_from_path():
-    # 從目錄路徑提取用戶名
-    return os.path.basename(os.path.expanduser('~'))
-
-def init_db():
-    # 初始化數據庫，創建留言表格
-    conn = sqlite3.connect(DATABASE_PATH)
-    c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS messages (username TEXT, message TEXT)')
-    conn.commit()
-    conn.close()
 
 # This is the entry point for jupyter-server-proxy . The packaging metadata
 # tells it about this function. For details, see:
@@ -44,41 +25,22 @@ def setup_hello():
 # something else outside of Python.
 # This example uses Python's low-level http.server, to minimise dependencies.
 class RequestHandler(BaseHTTPRequestHandler):
-    def do_POST(self):
-        # 處理提交的留言
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        message = urllib.parse.parse_qs(post_data.decode('utf-8'))['message'][0]
-        username = get_username_from_path()
-
-        # 存儲留言到數據庫
-        conn = sqlite3.connect(DATABASE_PATH)
-        c = conn.cursor()
-        c.execute('INSERT INTO messages (username, message) VALUES (?, ?)', (username, message))
-        conn.commit()
-        conn.close()
-
-        # 重定向回主頁
-        self.send_response(303)
-        self.send_header('Location', '/')
-        self.end_headers()
-
     def do_GET(self):
-        # 修改此方法以從數據庫讀取留言
-        messages = ""
-        conn = sqlite3.connect(DATABASE_PATH)
-        c = conn.cursor()
-        for row in c.execute('SELECT username, message FROM messages'):
-            messages += "<p><b>{}</b>: {}</p>".format(row[0], row[1])
-        conn.close()
+        server_addr = self.server.server_address
+        if isinstance(server_addr, tuple):
+            server_addr = "{}:{}".format(*server_addr)
 
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(TEMPLATE.format(
-            path=self.path, headers=self._headers_hide_cookie(),
-            server_address=server_addr, messages=messages,  # 將 messages 傳入模板
-        ).encode('utf-8'))
-        
+        try:
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(TEMPLATE.format(
+                path=self.path, headers=self._headers_hide_cookie(),
+                server_address=server_addr,
+            ).encode('utf-8'))
+        except BrokenPipeError:
+            # Connection closed without the client reading the whole response.
+            # Not a problem for the server.
+            pass
 
     def address_string(self):
         # Overridden to fix logging when serving on Unix socket
@@ -101,30 +63,14 @@ TEMPLATE = """\
 <!DOCTYPE html>
 <html>
 <head>
-<title>Hello Jupyter-server-proxy (YunLab)</title>
+<title> 公告區 </title>
 </head>
 <body>
-<h1>Hello Jupyter-server-proxy (YunLab)</h1>
-<p>Request path is <code>{path}</code></p>
-<p>Headers:</p>
-<pre>{headers}</pre>
-<p>Server is listening (behind the proxy) on <code>{server_address}</code>.</p>
+<h1>【YunLab書籤】</h1>
 <!-- 加入圖片超連結 -->
 <a href="https://memos.yunlab.synology.me/"  target="_blank">
     <img src="https://truth.bahamut.com.tw/s01/201610/88ba080e7c31f84a956be1e7861ccf28.JPG" alt="Linked Image"  title="Blog">
 </a>
-
-<!-- 表單增加在這裡 -->
-<form action="/" method="post">
-    <textarea name="message" placeholder="請輸入訊息"></textarea>
-    <br>
-    <input type="submit" value="Submit">
-</form>
-
-<!-- 留言顯示在這裡 -->
-<div id="messages">
-    {messages}
-</div>
 </body>
 </html>
 """
@@ -133,8 +79,6 @@ class HTTPUnixServer(HTTPServer):
     address_family = socket.AF_UNIX
 
 def main():
-    init_db()  # 確保數據庫和表格在伺服器啟動前被創建
-
     ap = argparse.ArgumentParser()
     ap.add_argument('-p', '--port')
     ap.add_argument('-u', '--unix-socket')
