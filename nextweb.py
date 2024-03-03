f28c1bcb0e1cd9564ba0f9e46a3cc4e529f27aab
arang129 <48246455+arang129@users.noreply.github.com>
2024-03-04 00:53:00 +0800

    Update nextweb.py

--- nextweb.py
+++ nextweb.py
@@ -7,7 +7,7 @@ from copy import copy
 from pathlib import Path
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-__version__ = '0.1'
+__version__ = '0.11'
 
 # This is the entry point for jupyter-server-proxy . The packaging metadata
 # tells it about this function. For details, see:
